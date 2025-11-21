9892be7b8943e035c8a69d00cb68efa73bcd533a
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-21 20:27:51 +0000

    add carousel to hero

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,12 +1,13 @@
 "use client";
 
 import BaseButton from "../buttons/BaseButton";
+import HeroCarousel from "../carousel/HeroCarousel";
 
 const Hero = () => {
   return (
     <div className="h-screen w-full relative flex flex-col justify-center items-center bg-gray-200">
       <h1>Welcome to Orange Hotels</h1>
-      {/* TODO: Carousel images on background */}
+      <HeroCarousel />
       <BaseButton onClick={() => alert("Booking now!")}>
         <span>Book Your Stay Now</span>
       </BaseButton>
