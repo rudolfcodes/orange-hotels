53215116dfda32b6f41f1b8adb15763840b8c8a9
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-21 20:26:47 +0000

    add slide images

---
+++ lib/carousel-slides/slides.ts
@@ -0,0 +1,9 @@
+const slides = [
+  { id: 1, image: "/images/lobby-slide.jpg" },
+  { id: 2, image: "/images/room-slide.jpg" },
+  { id: 3, image: "/images/bar-slide.jpg" },
+  { id: 4, image: "/images/gastronomy-slide.jpg" },
+  { id: 5, image: "/images/pool-slide.jpg" },
+];
+
+export default slides;
