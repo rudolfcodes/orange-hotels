9032388b5e4e60b2346db9b6d5ef9e6a3340e47c
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 17:09:46 +0000

    add hotelData

---
+++ app/api/hotels/hotels-data.ts
@@ -0,0 +1,7 @@
+const hotelData = [
+  { id: 1, name: "Orange Hotel Amsterdam", location: "Amsterdam" },
+  { id: 2, name: "Orange Hotel Rotterdam", location: "Rotterdam" },
+  { id: 3, name: "Orange Hotel Utrecht", location: "Utrecht" },
+];
+
+export { hotelData };
