03f497f44bd134487d96755b3e292051480b9026
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 11:33:15 +0000

    remove comments

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -6,13 +6,11 @@ import HeroCarousel from "../carousel/HeroCarousel";
 const Hero = () => {
   return (
     <div className="h-screen w-full absolute overflow-hidden flex flex-col max-h-[600px]">
-      {/* put a wrapper on top that ALIGNS everything with the logo */}
       <HeroCarousel />
       <div className="max-w-5xl w-full mx-auto h-full relative flex flex-wrap flex-col justify-center px-4 pointer-events-none">
         <h1 className="transform z-10 text-4xl md:text-6xl font-bold text-white drop-shadow-lg max-w-lg">
           Welcome to Orange Hotels
         </h1>
-        {/* button needs to be UNDER h1 */}
         <BaseButton
           className="pointer-events-auto"
           onClick={() => alert("Booking now!")}
