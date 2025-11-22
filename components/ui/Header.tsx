3e3844a303bd7fc925f6256455fee071c0d3652e
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 11:12:34 +0000

    add sticky header

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -5,10 +5,12 @@ import { menu } from "@/lib/menu";
 
 const Header = () => {
   return (
-    <div className="flex w-full justify-evenly h-16 bg-primary-orange items-center">
-      <h1>Orange Hotels</h1>
-      <Nav menu={menu} />
-      <button onClick={() => alert("Booking!")}>Book Now</button>
+    <div className="flex w-full h-16 items-center z-10 bg-dark-slate text-white sticky top-0">
+      <div className="max-w-5xl w-full mx-auto px-4 flex justify-between items-center">
+        <h1>Orange Hotels</h1>
+        <Nav menu={menu} />
+        <button onClick={() => alert("Booking!")}>Book Now</button>
+      </div>
     </div>
   );
 };
