b3465aeb4798bf5711602eff43cc55bb461fcb76
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 11:40:35 +0000

    place hero title & cta inside slide

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -1,23 +1,11 @@
 "use client";
 
-import BaseButton from "../buttons/BaseButton";
 import HeroCarousel from "../carousel/HeroCarousel";
 
 const Hero = () => {
   return (
     <div className="h-screen w-full absolute overflow-hidden flex flex-col max-h-[600px]">
       <HeroCarousel />
-      <div className="max-w-5xl w-full mx-auto h-full relative flex flex-wrap flex-col justify-center px-4 pointer-events-none">
-        <h1 className="transform z-10 text-4xl md:text-6xl font-bold text-white drop-shadow-lg max-w-lg">
-          Welcome to Orange Hotels
-        </h1>
-        <BaseButton
-          className="pointer-events-auto"
-          onClick={() => alert("Booking now!")}
-        >
-          Book Your Stay Now
-        </BaseButton>
-      </div>
     </div>
   );
 };
