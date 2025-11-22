a40fb447f3e09046d79928f2b40e44ea306f0b74
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 11:12:46 +0000

    remove unnecessary padding

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Hero from "@/components/ui/Hero";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+    <main className="flex min-h-screen flex-col items-center justify-between">
       <Header />
       <Hero />
       <Footer />
