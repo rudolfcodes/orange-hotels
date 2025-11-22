debdea6556f735edf33f839febc8e438f089e9f1
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 12:19:57 +0000

    translate route

--- lib/carousel-slides/slides.ts
+++ lib/carousel-slides/slides.ts
@@ -3,31 +3,31 @@ const slides = [
     id: 1,
     image: "/images/lobby-slide.jpg",
     title: "Welkom bij Orange Hotels",
-    cta: { text: "Reserveer nu", link: "/booking" },
+    cta: { text: "Reserveer nu", link: "/boeken" },
   },
   {
     id: 2,
     image: "/images/room-slide.jpg",
     title: "Ervaar comfort",
-    cta: { text: "Reserveer nu", link: "/booking" },
+    cta: { text: "Reserveer nu", link: "/boeken" },
   },
   {
     id: 3,
     image: "/images/bar-slide.jpg",
     title: "Ontspan in stijl",
-    cta: { text: "Reserveer nu", link: "/booking" },
+    cta: { text: "Reserveer nu", link: "/boeken" },
   },
   {
     id: 4,
     image: "/images/gastronomy-slide.jpg",
     title: "Geniet van gastronomie",
-    cta: { text: "Reserveer nu", link: "/booking" },
+    cta: { text: "Reserveer nu", link: "/boeken" },
   },
   {
     id: 5,
     image: "/images/pool-slide.jpg",
     title: "Ontspan bij het zwembad",
-    cta: { text: "Reserveer nu", link: "/booking" },
+    cta: { text: "Reserveer nu", link: "/boeken" },
   },
 ];
 
