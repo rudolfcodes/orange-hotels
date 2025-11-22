4d7c2f716707b247f3fd74aaf85e43f3990b9b21
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-22 11:33:04 +0000

    extend slides with title & cta

--- lib/carousel-slides/slides.ts
+++ lib/carousel-slides/slides.ts
@@ -1,9 +1,34 @@
 const slides = [
-  { id: 1, image: "/images/lobby-slide.jpg" },
-  { id: 2, image: "/images/room-slide.jpg" },
-  { id: 3, image: "/images/bar-slide.jpg" },
-  { id: 4, image: "/images/gastronomy-slide.jpg" },
-  { id: 5, image: "/images/pool-slide.jpg" },
+  {
+    id: 1,
+    image: "/images/lobby-slide.jpg",
+    title: "Welkom bij Orange Hotels",
+    cta: { text: "Reserveer nu", link: "/booking" },
+  },
+  {
+    id: 2,
+    image: "/images/room-slide.jpg",
+    title: "Ervaar comfort",
+    cta: { text: "Reserveer nu", link: "/booking" },
+  },
+  {
+    id: 3,
+    image: "/images/bar-slide.jpg",
+    title: "Ontspan in stijl",
+    cta: { text: "Reserveer nu", link: "/booking" },
+  },
+  {
+    id: 4,
+    image: "/images/gastronomy-slide.jpg",
+    title: "Geniet van gastronomie",
+    cta: { text: "Reserveer nu", link: "/booking" },
+  },
+  {
+    id: 5,
+    image: "/images/pool-slide.jpg",
+    title: "Ontspan bij het zwembad",
+    cta: { text: "Reserveer nu", link: "/booking" },
+  },
 ];
 
 export default slides;
