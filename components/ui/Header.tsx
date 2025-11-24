65b0d5a85aecd972ed1d012bfe62df3b1cd5b10b
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-24 20:59:02 +0000

    add CTA button background

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -3,17 +3,23 @@
 import Link from "next/link";
 import Nav from "./Nav";
 import { menu } from "@/lib/menu";
+import BaseButton from "../buttons/BaseButton";
 
 const Header = () => {
   /* TODO: opacity should be 0 on top with a black fading in gradient on the top. so black fades to 0 opacity. when scrolled it will be sticky */
   return (
-    <div className="flex w-full h-16 items-center z-10 bg-dark-slate text-white sticky top-0 min-h-20">
+    <div className="flex w-full h-16 items-center z-20 bg-dark-slate text-white sticky top-0 min-h-20">
       <div className="max-w-5xl w-full mx-auto px-4 flex justify-between items-center">
         <Link href="/">
           <h1>Orange Hotels</h1>
         </Link>
         <Nav menu={menu} />
-        <button onClick={() => alert("Booking!")}>Reserveer nu</button>
+        <BaseButton
+          className="bg-primary-orange p-4 rounded hover:bg-darker-orange transition-colors"
+          onClick={() => alert("Booking!")}
+        >
+          Reserveer nu
+        </BaseButton>
       </div>
     </div>
   );
