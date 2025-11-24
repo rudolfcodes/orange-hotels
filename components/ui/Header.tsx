e5d929316d0412a2ec529f1369266f7d8361716c
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-24 16:28:55 +0000

    make logo a link

--- components/ui/Header.tsx
+++ components/ui/Header.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import Link from "next/link";
 import Nav from "./Nav";
 import { menu } from "@/lib/menu";
 
 const Header = () => {
+  /* TODO: opacity should be 0 on top with a black fading in gradient on the top. so black fades to 0 opacity. when scrolled it will be sticky */
   return (
-    <div className="flex w-full h-16 items-center z-10 bg-dark-slate text-white sticky top-0">
+    <div className="flex w-full h-16 items-center z-10 bg-dark-slate text-white sticky top-0 min-h-20">
       <div className="max-w-5xl w-full mx-auto px-4 flex justify-between items-center">
-        <h1>Orange Hotels</h1>
+        <Link href="/">
+          <h1>Orange Hotels</h1>
+        </Link>
         <Nav menu={menu} />
         <button onClick={() => alert("Booking!")}>Reserveer nu</button>
       </div>
