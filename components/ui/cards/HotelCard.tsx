fe850933d2e37625f8e43c0ff19b9eb2236a0080
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-24 16:29:10 +0000

    add style fixes to HotelCard

--- components/ui/cards/HotelCard.tsx
+++ components/ui/cards/HotelCard.tsx
@@ -15,17 +15,23 @@ const HotelCard = ({
   return (
     <Link
       href={`/hotels/${hotelId}`}
-      className="bg-dark-slate flex-flex-col w-full p-3"
+      className="bg-dark-slate flex flex-col w-full p-5 max-w-70 rounded-lg hover:shadow-lg transition-shadow duration-300 text-soft-warm-white gap-2"
     >
-      <NextImage src={featuredImage} alt={title} />
-      <h2 className="text-xl font-bold mt-2">{title}</h2>
-      {location && <p className="text-sm text-gray-300">{location}</p>}
-      <p className="text-white mt-2 flex-1">{description}</p>
+      <NextImage
+        className="border-7 border-primary-orange"
+        width={280}
+        height={300}
+        src={featuredImage}
+        alt={title}
+      />
+      <h2 className="text-xl font-black mt-2">{title}</h2>
+      {location && <p className="text-sm text-soft-warm-white">{location}</p>}
+      <p className="text-soft-warm-white mt-2 flex-1">{description}</p>
       <div className="mt-4 flex items-center justify-between">
-        <span className="text-lg font-semibold">
+        <span className="text-lg font-semibold text-soft-warm-white">
           {currency} {pricePerNight} / night
         </span>
-        <span className="bg-orange-500 text-white px-2 py-1 rounded">
+        <span className="bg-orange-500 text-soft-warm-white px-2 py-1 rounded">
           {rating} ★
         </span>
       </div>
