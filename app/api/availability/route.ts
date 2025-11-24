85962158fd77bfef663f93ac3edf9ed923e6b1fe
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-24 13:08:20 +0000

    refactor route handler to filter hotels

--- app/api/availability/route.ts
+++ app/api/availability/route.ts
@@ -9,11 +9,20 @@ import {
   hasAtLeastOneGuest,
 } from "../../../types/booking/guards";
 import { hotelData } from "../hotels/hotels-data";
+import { filterHotels } from "@/lib/utils/filter-hotels";
+import { Hotel } from "@/types/hotel/hotel";
 
 export async function GET(request: NextRequest) {
   try {
     // Parse request body
     const body: BookingSearchInput = await request.json();
+    const searchParams = request.nextUrl.searchParams;
+    const checkIn = searchParams.get("checkIn");
+    const checkOut = searchParams.get("checkOut");
+    const adults = searchParams.get("adults");
+    const children = searchParams.get("children");
+    const hotelName = searchParams.get("hotelName");
+    const minRating = searchParams.get("minRating");
 
     if (!hasValidDateRange(body.dateRange)) {
       return NextResponse.json(
@@ -30,14 +39,28 @@ export async function GET(request: NextRequest) {
     }
 
     // Mock availability logic
-    const results: BookingSearchResult[] = hotelData.map((hotel) => ({
-      hotelId: hotel.id,
+    /* const results: BookingSearchResult[] = hotelData.map((hotel) => ({
+      hotelId: hotel.hotelId,
       hotelName: hotel.name,
       isAvailable: Math.random() > 0.5,
       pricePerNight: 100 + Math.floor(Math.random() * 200),
       currency: "EUR" as const, // Set currency as the exact literal type "EUR"
-    }));
-    return NextResponse.json(results);
+    })); */
+
+    const realResults: Hotel[] = filterHotels(hotelData, {
+      name: hotelName || undefined,
+      guestCount: {
+        adults: adults ? parseInt(adults) : 0,
+        children: children ? parseInt(children) : 0,
+      },
+      dateRange: {
+        checkIn: checkIn ? new Date(checkIn) : null,
+        checkOut: checkOut ? new Date(checkOut) : null,
+      },
+      minRating: minRating ? parseFloat(minRating) : undefined,
+    });
+
+    return NextResponse.json(realResults, { status: 200 });
   } catch (error) {
     return NextResponse.json(
       { error: "Internal Server Error" },
