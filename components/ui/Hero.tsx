92a855f77dc388454c9ab08380350be5ca39497e
Rudolf Cardol <r_cardol@hotmail.com>
2025-11-24 20:59:19 +0000

    fix carousel height

--- components/ui/Hero.tsx
+++ components/ui/Hero.tsx
@@ -4,7 +4,7 @@ import HeroCarousel from "../carousel/HeroCarousel";
 
 const Hero = () => {
   return (
-    <div className="h-screen w-full relative -mt-16 overflow-hidden flex flex-col max-h-[600px]">
+    <div className="h-screen w-full relative -mt-16 overflow-hidden flex flex-col max-h-[500px] lg:max-h-[600px]">
       <HeroCarousel />
     </div>
   );
